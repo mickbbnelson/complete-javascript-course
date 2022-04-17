3d91b9cb6670312fb2f9a40582cec17893b2649a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-04-17 13:26:58 -0600

    Fundemental refresh part 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,54 @@
+
+// # 1
+const country = 'USA';
+const continent = 'North America';
+let population = 329.5;
+
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// # 2
+
+const isIsland = false;
+let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// # 3
+
+language = 'English';
+
+population = 330;
+// console.log(population);
+
+// country = "Canada";
+// console.log(country);
+
+// # 4
+let halfPopulation = population / 2;
+// console.log(halfPopulation)
+
+let populationIncrease = ++population
+// console.log(populationIncrease)
+
+let finlandPop = 6
+
+const moreThanFinland = population >= finlandPop 
+// console.log(moreThanFinland)
+
+let averagePop = 33
+
+const moreThanAverage = population >= averagePop
+// console.log(moreThanAverage)
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+// console.log(description)
+
+const description2 = country + " is in " + continent + ", and its " + population + " million people speak " + language
+// console.log(description2)
+
